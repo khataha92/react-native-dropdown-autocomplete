bced7294ef4540e2a7938a2f86accc7d8d2d9bc0
Khalid Taha <kha.taha92@gmail.com>
2019-12-24 15:57:10 +0200

    Update index.js

--- components/Autocomplete/index.js
+++ components/Autocomplete/index.js
@@ -195,6 +195,7 @@ class Autocomplete extends Component {
               if (scrollToInput) {
                 scrollToInput(findNodeHandle(event.target));
               }
+              this.handleInputChange("");
             }}
           />
           {loading && (
